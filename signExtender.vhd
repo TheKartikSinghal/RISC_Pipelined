881da8fef2e4a84a47fe4560b688f5021f64e396
Kartik Singhal <singhal.kartik1234@gmail.com>
2023-04-25 09:52:51 +0530

    sign extender changes

--- signExtender.vhd
+++ signExtender.vhd
@@ -3,9 +3,9 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity SignExtender is
-    port(nine_bit_in : in std_logic_vector(8 downto 0);
+    port(input : in std_logic_vector(8 downto 0);
     OpCode: in std_logic_vector(3 downto 0);
-    sixteen_bit_out : out std_logic_vector(15 downto 0));
+    output : out std_logic_vector(15 downto 0));
 end entity;
 
 architecture arch_SignExtender of SignExtender is 
@@ -16,19 +16,19 @@ begin
     p1: process(clk)
     begin
     if (OpCode="0000" || OpCode="0100" ||OpCode="0101" || OpCode="1000" ||OpCode="1001") then
-        sixteen_bit_out(5 downto 0) <= nine_bit_in(5 downto 0);
+        output(5 downto 0) <= input(5 downto 0);
         fill: for i in 6 to 15 generate
-            sixteen_bit_out(i) <= nine_bit_in(6);
+            output(i) <= input(6);
         end generate;
     elsif (OpCode="1100" ||OpCode="1111") then
-        sixteen_bit_out(8 downto 0) <= nine_bit_in(8 downto 0);
+        output(8 downto 0) <= input(8 downto 0);
         fill: for i in 9 to 15 generate
-            sixteen_bit_out(i) <= nine_bit_in(8);
+            output(i) <= input(8);
         end generate;
     elsif (OpCode= "0011") then
-        sixteen_bit_out(8 downto 0) <= nine_bit_in(8 downto 0);
+        output(8 downto 0) <= input(8 downto 0);
         fill: for i in 9 to 15 generate
-            sixteen_bit_out(15 downto 9) <= "000000");
+            output(15 downto 9) <= "000000");
         end generate;
         end if;
 
